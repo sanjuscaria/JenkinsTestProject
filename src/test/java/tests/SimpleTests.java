059b3a79f57cf1477c3d57ba1eb7f8c41dd46197
sanju <sanju.scaria@gmail.com>
2025-05-30 16:59:05 +0530

    commiting changes after adding SimpleTests

---
+++ src/test/java/tests/SimpleTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.annotations.*;
+
+public class SimpleTests {
+
+	@Test	
+	public void test01()
+	{
+		System.out.println("line number 1");
+		System.out.println("line number 2");
+		System.out.println("line number 3");
+		System.out.println("line number 4");
+		System.out.println("line number 5");
+		System.out.println("line number 6");
+	}
+	
+	
+}
